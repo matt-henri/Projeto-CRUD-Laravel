060c864a5bbe790df9668d83cc9224041b7f0f1e
Mateus Henrique <mathmarkub@gmail.com>
2025-11-30 13:29:53 -0300

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => '',
+            'name' => 'Mateus',
             'email' => '',
         ]);
     }
